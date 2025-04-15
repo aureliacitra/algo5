ade414e6836648540e142592ba434787c849f470
aureliacitra <citrapangukir@gmail.com>
2025-04-15 11:13:10 +0700

    add if

--- mergesort.cpp
+++ mergesort.cpp
@@ -10,5 +10,8 @@ void input()
     while (true) {
         cout << "Enter the number of elements in the array: ";
         cin >> n;
+        if (n <= 20) break;
+        else cout << "Please enter a number less than or equal to 20." << endl;
     }
+
     
